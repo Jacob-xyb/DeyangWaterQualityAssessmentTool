eb0da122c7fc22f95199ea367334bbca00e8fc8f
Ethnn <812537071qq@gmail.com>
2020-11-26 09:26:49 +0800

    completed

--- master/deyang_band_v1.0.py
+++ master/deyang_band_v1.0.py
@@ -22,7 +22,7 @@ def read_tiff(path, key = None):
     data_list = []
     for i in range(nYSize):
         for j in range(nXSize):
-            if data[i, j] > 0:
+            if data[i, j] >= 0:
                 data_list.append(data[i, j])
             elif key == 1:
                 if data[i, j] > -1:
@@ -31,7 +31,6 @@ def read_tiff(path, key = None):
                     data_list.append(-2)
             else:
                 data_list.append(-999)
-    print(str(nXSize)+","+str(nYSize))
     return data_list
 
 def read_xy(path):
@@ -69,12 +68,13 @@ def read_xy(path):
 
 
 if __name__ == '__main__':
-    path_tiff = r"/Users/ethan/Desktop/newfiber/xyb/哨兵_2020_10m_cgcs2000"
-    path1 = r"/s2b20200216waterRrs_CHLAz.tif"
-    path2 = r"/s2b20200216waterRrs_SDz.tif"
-    path3 = r"/s2b20200216waterRrs_TNz.tif"
-    path4 = r"/s2b20200216waterRrs_TPz.tif"
-    path5 = r"/s2b20200216waterRrs_NDVIz.tif"
+    path_tiff = r"/Users/ethan/Desktop/newfiber/xyb/德阳/20200317"
+    path1 = r"/s2b20200317waterRrs_CHLAz.tif"
+    path2 = r"/s2b20200317waterRrs_SDz.tif"
+    path3 = r"/s2b20200317waterRrs_TNz.tif"
+    path4 = r"/s2b20200317waterRrs_TPz.tif"
+    path5 = r"/s2b20200317waterRrs_NDVIz.tif"
+    path6 = r"/s2b20200317waterRrs_FAI01z.tif"
 
     XY = read_xy(path_tiff+path1)
     cdnX, cdnY = XY[0], XY[1]
@@ -83,12 +83,14 @@ if __name__ == '__main__':
     TN = read_tiff(path_tiff+path3)
     TP = read_tiff(path_tiff+path4)
     NDVI = read_tiff(path_tiff+path5, 1)
-    print(cdnX[0])
+    FAI01 = read_tiff(path_tiff+path6)
+
+    # print(cdnX[0])
     # 创建DataFrame
     # data n= [cdnX, cdnY, CHLA, SD, TN, TP]
     # columns = ['X', 'Y', 'CHLA', 'SD', 'TP', 'TN']
     dict = {'X': cdnX, 'Y': cdnY, 'CHLA': CHLA,
-            'SD': SD, 'TP': TP, 'TN': TN, 'NDVI':NDVI}
+            'SD': SD, 'TP': TP, 'TN': TN, 'NDVI':NDVI, 'FAI01':FAI01}
     df = pd.DataFrame(data=dict)
     # df.drop(index=df[df['CHLA'].isin([-999])].index[0], inplace=True)  # 只删除了一行
     # df['CHLA'].isin([-999])  # Name: CHLA, Length: 210748, dtype: bool
@@ -97,18 +99,19 @@ if __name__ == '__main__':
     # df.drop(index=(df.loc[(df['CHLA'] == -999)].index), inplace=True)  # 可删除多行 # 只允许有一个key
     # 清洗异常值
     for col in df.columns:  # df1.columns : 列名称的list
-        # print(col)  # col为一个个列名
+        #print(col)  # col为一个个列名
         #df.drop(index=(df.loc[(df[col] == -999)].index), inplace=True)
-        df.drop(index=(df[df[col].isin([-999])].index), inplace=True)
+        df.drop(index=(df[df[col].isin([-999])].index), inplace=True) #清洗4个指标
+        df.drop(index=(df[df[col].isin([-2])].index), inplace=True) #清洗NDVI
         # df.reset_index(inplace=True)  # 重置索引，但会保留原索引
-        #df.reset_index(drop=True, inplace=True)  # 重置索引，不会保留原索引
-    print(df)
+        df.reset_index(drop=True, inplace=True)  # 重置索引，不会保留原索引
+    # print(df)
 
     """写入部分"""
-    """writer = pd.ExcelWriter(r'../../s2b_20200216_waterRrs2.xlsx')  # 写入Excel文件
+    writer = pd.ExcelWriter(r'../../../德阳/s2b_20200317_waterRrs.xlsx')  # 写入Excel文件
     df.to_excel(writer, float_format='%.5f')  # ‘page_1’是写入excel的sheet名 # 不写就是默认第一页
     writer.save()
-    writer.close()"""
+    writer.close()
 
 
 
