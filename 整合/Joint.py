54af7e2c9a0ea083d56ce3d14770a8820f9417ee
wmj <123456>
2020-12-28 11:08:26 +0800

    12.28 commit

--- 整合/Joint.py
+++ 整合/Joint.py
@@ -22,18 +22,21 @@ if __name__ == '__main__':#C:\Users\Administrator\PycharmProjects\input_param.xl
     #多波段三张表，一张参数表，两张测评表，其中参数表多两列NDVI 和 FAI
     else:
         print("输入有误")
-    #最后一步
-    #file1="./" + ref.iat[14, 2] + "NDVI&FAI.xlsx"#单波段没有这个文件就报错
-    #file2="./" + ref.iat[14, 2] + ".xlsx"
-    #df1 = pd.read_excel(file1, index_col=0)
-    #df2 = pd.read_excel(file2, index_col=0)
-    #df2_selected = df2[df2["FAI01"] == 1]
-    #count = len(df2_selected)  # df2中1的个数
-    #print(count)
-    #df1_selected = df1.nlargest(count, "FAI")  # df1中top个数与df2中1的个数一致
+    '''
+    file1 = 'E://shixi//github_mycellar//DeyangWaterQualityAssessmentTool//整合//多波段图计算结果.xlsx'  # 自己跑出来要检验结果的文件
+    file2 = "E://shixi//github_mycellar//DeyangWaterQualityAssessmentTool//整合//s2b_20200317_waterRrs.xlsx" # 只含1，0的"原文件"
+    # df = read_band(path=path_band, head=200)
+    df1 = pd.read_excel(file1, index_col=0)
+    df2 = pd.read_excel(file2, index_col=0)  # 只含1，0的那个文件
+    #处理部分
+    df2_selected = df2[df2["FAI01"] == 1]
+    count = len(df2_selected)  # df2中1的个数
+    df1_selected = df1.nlargest(count, "FAI")  # df1中top个数与df2中1的个数一致
     # inner join：
-    #df_merge = pd.merge(df1_selected[['X', 'Y']], df2_selected[['X', 'Y']], on=['X', 'Y'], how='inner')  # 坐标匹配的条目
+    df_merge = pd.merge(df1_selected[['X', 'Y']], df2_selected[['X', 'Y']], on=['X', 'Y'], how='inner')  # 坐标匹配的条目
     # df_merge = df1[df1["FAI"] >= 0] # 直接看大于0的条目
     # print(len(df_merge))
-    #prop = len(df_merge) / count
-    #print(df1_selected["FAI"])
+    prop = len(df_merge) / count
+    print(df1_selected["FAI"])
+    '''
+
